d31cd7d7fa25a876acf0afd3c463b9cf3e8ea9e6
Alpha Olomi <alphaolomi@gmail.com>
2020-09-16 10:39:47 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {Client} from 'africastalking-ts'
 
 async function run(): Promise<void> {
   try {
-    // Get AT API Keys from Inputs
+    // Get AT API Keys from     
     const AT_API_KEY = core.getInput('AT_API_KEY')
     const AT_USERNAME = core.getInput('AT_USERNAME')
     const AT_MOBILE_NUMBER = core.getInput('AT_MOBILE_NUMBER')
@@ -21,10 +21,10 @@ async function run(): Promise<void> {
     client
       .sendSms({
         to: [AT_MOBILE_NUMBER],
-        message: `Hello, ${payload}`
+        message: `Hello, a new commit`
       })
       .then(response => {
-        console.log(response)
+        console.log(`Response: ${response}`)
         core.setOutput('message_status', response)
       })
       .catch(error => console.log(error))
