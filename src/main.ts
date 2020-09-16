2a20a89c4732ab1ea7af8028878cb4e801752fdc
Alpha Olomi <alphaolomi@gmail.com>
2020-09-16 06:36:16 +0300

    feat: add sms functionality

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,35 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {Client} from 'africastalking-ts'
 
 async function run(): Promise<void> {
   try {
-    const nameToGreet = core.getInput('who-to-greet')
-
-    console.log(`Hello ${nameToGreet}!`)
-    const time = new Date().toTimeString()
-
-    // make output
-    core.setOutput('time', time)
+    // Get AT API Keys from Inputs
+    const AT_API_KEY = core.getInput('AT_API_KEY')
+    const AT_USERNAME = core.getInput('AT_USERNAME')
+    const AT_MOBILE_NUMBER = core.getInput('AT_MOBILE_NUMBER')
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
 
-    //
+    // Send Message
+    const client = new Client({
+      apiKey: AT_API_KEY,
+      username: AT_USERNAME
+    })
+
+    client
+      .sendSms({
+        to: [AT_MOBILE_NUMBER],
+        message: `Hello, ${payload}`
+      })
+      .then(response => {
+        console.log(response)
+        core.setOutput('message_status', response)
+      })
+      .catch(error => console.log(error))
+
+    // Log event to console
     console.log(`The event payload: ${payload}`)
   } catch (error) {
     core.setFailed(error.message)
